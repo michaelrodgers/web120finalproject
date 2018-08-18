7f723f78adbfd847502b7ea3ab29afee39c4319e
michaelrodgers <38140530+michaelrodgers@users.noreply.github.com>
2018-08-18 01:05:39 -0700

    Add files via upload

---
+++ fp/index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="robots" content="follow, index" />
+<meta property="og:site_name" content="Deja K Taylor Official Website" />
+<meta property="og:title" content="Deja K Taylor Official Website" />
+<meta property="og:description" content="News, Music, Tour Dates, Merch, and More" />
+<meta name="description" content="News, Music, Tour Dates, Merch, and More" />
+<meta property="og:type" content="website" />
+<link rel="stylesheet" href="../fp/css/nav.css" type="text/css"/>
+<link rel="stylesheet" href="../fp/css/fp.css" type="text/css"/>   
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> 
+<title>Deja K. Taylor Official Site</title>
+</head>
+<body>
+    <a href="index.php" class="banner"><h1 name=top>Deja K Taylor</h1></a>
+    <nav>
+        <ul>
+            <li><a href=#featuredvieo>Featured Video</a></li>
+            <li><a href=#imagegallery>Image Gallery</a></li>
+            <li><a href=#upcomingshows>Upcoming Shows</a></li>
+            <li><a href=#videos>Videos</a></li>
+            <li><a href=#musicplaylist>Music Playlist</a></li>
+            <li><a href=#fashionmerchandise>Fashion Merchandis</a></li>
+            <li><a href=#socialmedialinks>Social Media Links</a></li>
+            <li><a href=#businesscontact>Business Contact</a></li>
+        </ul>
+    </nav>
+    <h2 name="featuredvideo">Superbad Ft. King</h2>
+    <iframe src="https://www.youtube.com/embed/7iw64TnQNGc" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <div class="wrap">
+    <div class="frame" id="basic" name="imagegallery">
+                <ul>
+                <li><figure>
+                    <a href="images/Deja1.jpg" data-lightbox="deja" data-title="Denim & Mesh"><img src="images/Deja1.jpg" alt="Denim & Mesh" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja2.jpg" data-lightbox="deja" data-title="Superbad"><img src="images/Deja2.jpg" alt="Superbad" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja4.jpg" data-lightbox="deja" data-title="Cruella"><img src="images/Deja4.jpg" alt="Cruella" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja5.jpg" data-lightbox="deja" data-title="Framed"><img src="images/Deja5.jpg" alt="Framed" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja6.jpg" data-lightbox="deja" data-title="The Inconvenience"><img src="images/Deja6.jpg" alt="The Inconvenience" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja7.jpg" data-lightbox="deja" data-title="Dark Deja"><img src="images/Deja7.jpg" alt="Dark Deja" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja8.jpg" data-lightbox="deja" data-title="Chi-blaq"><img src="images/Deja8.jpg" alt="Chi-blaq" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja9.jpg" data-lightbox="deja" data-title="McMadam"><img src="images/Deja9.jpg" alt="McMadam" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja10.jpg" data-lightbox="deja" data-title="Wrapped Up, But Never Pressed"><img src="images/Deja10.jpg" alt="Wrapped Up, But Never Pressed" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja11.jpg" data-lightbox="deja" data-title="Hues of Blue"><img src="images/Deja11.jpg" alt="Hues of Blue" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja12.jpg" data-lightbox="deja" data-title="Glamour"><img src="images/Deja12.jpg" alt="Glamor" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja13.jpg" data-lightbox="deja" data-title="Dancing With Dipper"><img src="images/Deja13.jpg" alt="Dancing With Dipper" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja14.jpg" data-lightbox="deja" data-title="With Atukwe"><img src="images/Deja14.jpg" alt="With Atukwe" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja15.jpg" data-lightbox="deja" data-title="AfroPunk"><img src="images/Deja15.jpg" alt="AfroPunk" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja16.jpg" data-lightbox="deja" data-title="Through the Looking Glass"><img src="images/Deja16.jpg" alt="Through the Looking Glass" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja17.jpg" data-lightbox="deja" data-title="Tres"><img src="images/Deja17.jpg" alt="Tres" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja18.jpg" data-lightbox="deja" data-title="Lyric Vixen"><img src="images/Deja18.jpg" alt="Lyric Vixen" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja19.jpg" data-lightbox="deja" data-title="Auburn"><img src="images/Deja19.jpg" alt="Auburn" /></a>
+                </figure></li>
+                <li><figure>
+                    <a href="images/Deja20.jpg" data-lightbox="deja" data-title="Summertime Fresh Fish"><img src="images/Deja20.jpg" alt="Summertime Fresh Fish" /></a>
+                </figure></li>
+                </ul>
+        </div>
+    </div>
+        
+        <h2 name="upcomingshows" class="gridheader">Upcoming Shows</h2>
+        <table>
+	 		<thead>
+				<tr>
+						<th class="col1">MM_DD</th>
+						<th>LOCATION_VENUE</th>
+						<th>FEATURING...</th>
+						<th>TICKETS</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>08_18</td>
+					<td>
+                        <p>The Novo</p>
+                        <p><a href="https://www.google.com/maps/place/The+Novo/@34.0445171,-118.2655281,15z/data=!4m5!3m4!1s0x0:0xda9785bec3c53630!8m2!3d34.0445171!4d-118.2655281">800 W Olympic Blvd, Los Angeles, CA 90015</a></p>
+                    </td>
+					<td><a href="https://www.facebook.com/DJQuik?ref=ts&fref=ts">DJ Quik</a></td>
+					<td><a href="https://www.axs.com/events/355435/dj-quik-playing-rhythm-al-ism-tickets?skin=novo&src=AEGLIVE_WCLUNLAX030115VEN001">Tickets</a></td>
+				</tr>
+				<tr>
+					<td>08_24</td>
+					<td>
+                        <p>Art Walk Lounge</p>
+                        <p><a href="https://www.google.com/maps/place/Art+Walk+Lounge/@34.0449819,-118.2514446,15z/data=!4m5!3m4!1s0x0:0xdfa0946da2cabf1a!8m2!3d34.0449819!4d-118.2514446?hl=en">634 S Spring St, Los Angeles, CA 90014</a></p>
+                    </td>
+					<td>N/A</td>
+					<td>Free to Public</td>
+				</tr>
+				<tr>
+					<td>08_25</td>
+					<td>
+                        <p>Whisky A Go Go</p>
+                        <p><a href="https://www.google.com/maps/place/Whisky+a+Go+Go/@34.090848,-118.3857624,15z/data=!4m2!3m1!1s0x0:0xaf95e77a7b374a71?hl=en&sa=X&sqi=2&ved=2ahUKEwjWq8yNh9rcAhWnDcAKHaksDM4Q_BIwDnoECAoQCw">8901 Sunset Blvd, West Hollywood, CA 9006</a></p>
+                    </td>
+					<td><a href="http://davebrock.com/">Dave Brock</a></td>
+					<td><a href="http://www.wildchild.info/">Tickets</a></td>
+				</tr>
+				<tr>
+					<td>09_08</td>
+					<td>
+                        <p>The Observatory</p>
+                        <p><a href="https://www.google.com/maps/place/The+Observatory/@33.6998598,-117.9175784,15z/data=!4m5!3m4!1s0x0:0x8d66acc3edb9c1c1!8m2!3d33.6998598!4d-117.9175784?hl=en">3503 S Harbor Blvd, Santa Ana, CA 92704</a></p>
+                    </td>
+					<td><a href="https://thefrights.com/">The Frights</a></td>
+					<td><a href="https://www.ticketweb.com/event/the-frights-hunny-the-observatory-tickets/8477005?pl=observatory&REFID=clientsitewp">Tickets</a></td>
+				</tr>
+				<tr>
+					<td>09_15</td>
+					<td>
+                        <p>Lodge Room</p>
+                        <p><a href="https://www.google.com/maps/place/Lodge+Room/@34.1091167,-118.1945691,15z/data=!4m2!3m1!1s0x0:0x2decb61679954f31?hl=en&sa=X&ved=2ahUKEwjY9PKiidrcAhXAIjQIHTgbDKUQ_BIwEHoECAgQCw">104 N. Ave 56, 2nd Floor, Los Angeles, CA 90042</a></p>
+                    </td>
+					<td>N/A</td>
+					<td><a href="https://lodgeroomhlp.com/">Tickets</a></td>
+                </tr>
+			</tbody>
+	   </table>
+    
+       <h2 id="video">Video</h2>
+       <div class="clear">
+       <iframe src="https://www.youtube.com/embed/BkJ9t5j82Cw" allow="autoplay; encrypted-media" allowfullscreen class="videolist"></iframe>
+       <iframe src="https://www.youtube.com/embed/EIVJo-5BLDE" allow="autoplay; encrypted-media" allowfullscreen class="videolist"></iframe>
+       <iframe src="https://www.youtube.com/embed/4Hs3e529MAg" allow="autoplay; encrypted-media" allowfullscreen class="videolist"></iframe>
+       </div>
+    
+       <h2 name="musicplaylist">SoundCloud Playlist</h2>
+    
+       <table class="scplaylist">
+	 		<thead>
+				<tr>
+						<th class="col1">Playlist</th>
+						<th>Purchase Album</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><iframe height="300" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/148000363&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></td>
+					<td></td>
+				</tr>
+                <tr>
+					<td><iframe height="300" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/335195052&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></td>
+					<td></td>
+				</tr>
+                <tr>
+					<td><iframe height="300" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/116494248&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></td>
+					<td></td>
+				</tr>
+                <tr>
+					<td><iframe height="300" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/190722948&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></td>
+					<td></td>
+				</tr>
+            </tbody>
+	   </table>
+    
+       <h2 name="fashionmerchandise">Fashion Merchandise</h2>
+       <div class="clear">
+       <a href="https://www.tradesy.com/i/walter-steiger-blue-with-white-polka-dots-and-red-heel-navy-unique-pumps-size-us-9-regular-m-b/23006381/?utm_source=bpl&utm_medium=cpc&utm_campaign=Shopping%20-%20Shoes%20-%20Pumps%20-%20Brand%20-%20FTB&utm_content=all%20other%20brands%20-%20pumps&utm_term=&msclkid=bb8e02c84ae218661b1159a16e6277b5"><img src="images/fashion1.jpg" alt="polka dot pumps"></a>
+       <a href="http://nowlet.com/product/productDetail?productNo=12253_5673&ranMID=42696&ranEAID=tZkYzve9Cvk&ranSiteID=tZkYzve9Cvk-5cjUvZWaZGVo0QuDFW1tUw"><img src="images/fashion2.jpg" alt="raf simmons adidas"></a>   
+       <a href="https://www.net-a-porter.com/us/en/product/1068232?cm_mmc=LinkshareUS-_-tZkYzve9Cvk-_-Custom-_-LinkBuilder&siteID=tZkYzve9Cvk-90Xg0pmhFecP8c.Y1o7.jw&Lyst+US=Lyst+US"><img src="images/fashion3.jpg" alt="houndstooth blazer"></a>
+       <a href="https://www.tradesy.com/i/charles-david-black-collection-bootsbooties-size-eu-35-approx-us-5-regular-m-b/22594304/?utm_source=bpl&utm_medium=cpc&utm_campaign=Shopping%20-%20Shoes%20-%20Boots%20%26%20Booties%20-%20Brand%20-%20FTB&utm_content=all%20other%20brands%20-%20boots&utm_term=&msclkid=a7e707cce23b1cfb21415ca5c71a1f35"><img src="images/fashion4.jpg" alt="knee-high boots"></a>
+       <a href="https://www.yoox.com/US/41817034/item?gender=D&cod10=41817034FQ&tp=23157&kpid=US-41817034FQ-4-AI18&adtype={adtype}&mrkgcl=976&mrkgadid=3289021889&utm_term=ProductType1womensProductType2coats_%26_jackets&tp=167509&creative=76622268105951&device=c&matchtype=e"><img src="images/fashion5.jpg" alt="wild patterned puffy jacket"></a>
+       <a href="https://shop.nordstrom.com/s/comme-des-garcons-x-nike-heeled-bootie-women/4887171?&utm_content=1265538031738389_6fa1b932-cee3-4b15-a4f4-375bfb4aa37e&utm_term=pla-4582695746545132&utm_channel=shopping_ret_p&sp_source=bing&sp_campaign=291441558&utm_source=bing&utm_medium=cpc&utm_campaign=Women%20-%20Generic%20-%20Bing"><img src="images/fashion6.jpg" alt="nike heel"></a>
+       <a href="https://www.yoox.com/us/39792690FC/item?utm_campaign=tZkYzve9Cvk&siteID=tZkYzve9Cvk-ecr3.kO0nfgd2E7O__HL1g&utm_content=10&utm_medium=affiliazione&utm_source=linkshare_us&ranMID=24285&ranEAID=tZkYzve9Cvk&ranSiteID=tZkYzve9Cvk-ecr3.kO0nfgd2E7O__HL1g&tp=11333#cod10=39792690FC&sizeId=3&sizeName=2"><img src="images/fashion7.jpg" alt="full length black twill jacket with green lapel"></a>
+       <a href="https://www.neimanmarcus.com/p/dries-van-noten-ruched-long-sleeve-techno-satin-top-prod212280454?ecid=NMCS__BingPLA&utm_medium=CSE&utm_source=NMCS__BingPLA&utm_campaign=DriesVanNoten&msclkid=3d2eeffbc5a410539541b492e08bd142"><img src="images/fashion8.jpg" alt="satin blouse"></a>
+       </div>
+       
+       <h2 name=socialmedialinks>Social Media Links</h2>
+            <div class="social">
+            <a href="https://www.facebook.com/DejaKTaylor/" title="Deja's Facebook"><i class="fa fa-fw fa-facebook"></i></a>
+            <a href="https://twitter.com/dejaktaylor?lang=en" title="Deja's Twitter"><i class="fa fa-fw fa-twitter"></i></a>
+            <a href="https://www.instagram.com/madamsuperbad/"  title="Deja's Instagram"><i class="fa fa-fw fa-instagram"></i></a>
+            <a href="https://www.youtube.com/user/FREEDEJATAYLOR"  title="Deja's YouTube"><i class="fa fa-fw fa-youtube"></i></a>
+            </div>
+    
+       <h2 name=businesscontact>Business Inquries</h2>
+            <div class="clear">
+            <?php include 'includes/multiple.php';?>
+                <p class="clear-recaptcha"></p>
+            </div>
+    
+    <footer>
+            <p><small>&copy; August, 9 2018 by <a href="contact.php" target="_blank">Deja K Taylor</a>, All Rights Reserved ~ <a href="https://validator.w3.org/check?uri=https://michaelrodgers.azurewebsites.net/fp/index.php" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fmichaelrodgers.azurewebsites.net%2Ffp%2Findex.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en#errors" target="_blank">Valid CSS</a></small></p>
+            <p class="disclaimer">The information contained in this website is for general information purposes only. The information is provided by Deja K. Taylor and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+            In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+            Through this website you are able to link to other websites which are not under the control of Deja K Taylor. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+            Every effort is made to keep the website up and running smoothly. However, Deja K. Taylor takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
+            </p>
+        </footer>
+    <p><a href=#top title="back to the top">Back to the top</a></p>
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
+            }
+        </script>
+    
+</body>
+</html>
---
+++ fp/js/script.js
@@ -0,0 +1,11 @@
+(function($){
+$(document).ready(function(){
+
+$("#cssmenu").menumaker({
+    title: "Menu",
+    breakpoint: 1102,
+    format: "multitoggle"
+});
+
+});
+})(jQuery);
